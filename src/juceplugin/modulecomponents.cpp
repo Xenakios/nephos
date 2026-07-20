542c09d4ca993e9d4136b8465959ac3dba1d6a38
xenakios <xenakios@gmail.com>
2026-07-20 15:44:15 +0300

    more moving to cpp

--- src/juceplugin/modulecomponents.cpp
+++ src/juceplugin/modulecomponents.cpp
@@ -503,3 +503,104 @@ bool OscTypeComponent::keyPressed(const juce::KeyPress &ev)
     }
     return false;
 }
+void OscTypeComponent::paint(juce::Graphics &g)
+{
+    g.fillAll(juce::Colours::black);
+    int otypebase = *processorRef.granulator.idtoparvalptr[ToneGranulator::PAR_OSCTYPE];
+    float cellw = 50.0f;
+    for (int i = 0; i < 7; ++i)
+    {
+        if (i == otypebase)
+            g.setColour(juce::Colours::yellow.darker());
+        else
+            g.setColour(juce::Colours::darkgrey);
+        juce::Rectangle<float> r{cellw * i + 2.0f, 2.0f, cellw - 4.0f, cellw - 4.0f};
+        g.fillRect(r);
+
+        g.setColour(juce::Colours::white);
+        int mappedtype = processorRef.granulator.osctypemapping[i];
+        if (mappedtype >= 0 && mappedtype < waveimages.size())
+        {
+            g.drawImage(waveimages[mappedtype], cellw * i + 2.0, 2.0f, cellw - 4.0f, cellw - 4.0f,
+                        0, 0, 50, 50);
+        }
+        if (i == priorOscType)
+        {
+            g.setColour(juce::Colours::green);
+            g.fillRect(cellw * i + 4.0f, 4.0f, 8.0f, 8.0f);
+        }
+        // g.drawFittedText(waveshortnames[mappedtype], r.getX(), r.getY(), r.getWidth(),
+        //                  r.getHeight(), juce::Justification::centred, 1);
+    }
+}
+void OscTypeComponent::mouseDown(const juce::MouseEvent &ev)
+{
+    int otype = ev.x / 50.0;
+    if (otype >= 0 && otype < 7)
+    {
+        if (!ev.mods.isRightButtonDown())
+        {
+            ParameterMessage msg;
+            msg.id = ToneGranulator::PAR_OSCTYPE;
+            msg.value = otype;
+            processorRef.params_from_gui_fifo.push(msg);
+        }
+        else
+        {
+            juce::PopupMenu menu;
+            for (int i = 0; i < 7; ++i)
+            {
+                auto dr = std::make_unique<juce::DrawableImage>(waveimages[i]);
+                menu.addItem(juce::PopupMenu::Item(waveshortnames[i])
+                                 .setImage(std::move(dr))
+                                 .setID(i + 1)
+                                 .setAction([this, otype, i] {
+                                     processorRef.granulator.osctypemapping[otype] = i;
+                                     repaint();
+                                 }));
+            }
+            menu.showMenuAsync(juce::PopupMenu::Options{}.withStandardItemHeight(50));
+        }
+    }
+    repaint();
+}
+void OscillatorModuleComponent::resized()
+{
+    oscTypeComponent.setBounds(7, 17, 350, 50);
+    oscPitchKnob.setBounds(7, oscTypeComponent.getBottom() + 1, 80, 100);
+
+    for (int i = 0; i < modDepthKnobs.size(); ++i)
+    {
+        modDepthKnobs[i]->setBounds(oscPitchKnob.getRight() + 2,
+                                    oscTypeComponent.getBottom() + 1 + i * 51, 80, 50);
+    }
+
+    // pitchEnvWarpKnob.setBounds(oscPitchKnob.getRight() + 2, pitchEnvKnob.getBottom() + 1, 80,
+    //                            50);
+    pitchEnvelopeComponent.setBounds(modDepthKnobs[0]->getRight() + 2, oscTypeComponent.getBottom(),
+                                     150, 150);
+    grainModComponent.setBounds(pitchEnvelopeComponent.getRight() + 2, oscTypeComponent.getBottom(),
+                                150, 150);
+    oscSyncKnob.setBounds(grainModComponent.getRight() + 2, oscTypeComponent.getBottom() + 1, 80,
+                          50);
+    oscPWKnob.setBounds(grainModComponent.getRight() + 2, oscSyncKnob.getBottom() + 1, 80, 50);
+    oscFMPitchKnob.setBounds(oscSyncKnob.getRight() + 2, oscTypeComponent.getBottom() + 1, 80, 50);
+    oscFMDepthKnob.setBounds(oscFMPitchKnob.getRight() + 2, oscTypeComponent.getBottom() + 1, 80,
+                             50);
+    oscFMFeedbackKnob.setBounds(oscFMDepthKnob.getRight() + 2, oscTypeComponent.getBottom() + 1, 80,
+                                50);
+    oscNoiseModeDrop.setBounds(oscSyncKnob.getRight() + 2, oscFMPitchKnob.getBottom() + 1, 250, 25);
+    oscNoiseCorrelationKnob.setBounds(oscSyncKnob.getRight() + 2, oscNoiseModeDrop.getBottom() + 1,
+                                      80, 50);
+}
+void StackingModuleComponent::resized()
+{
+    juce::FlexBox flex;
+    flex.flexDirection = juce::FlexBox::Direction::row;
+    flex.items.add(juce::FlexItem(countKnob).withFlex(1.0).withMargin(2));
+    flex.items.add(juce::FlexItem(lengthKnob).withFlex(1.0).withMargin(2));
+    flex.items.add(juce::FlexItem(warpKnob).withFlex(1.0).withMargin(2));
+    flex.items.add(juce::FlexItem(pitchRandomKnob).withFlex(1.0).withMargin(2));
+    flex.items.add(juce::FlexItem(spatRandomKnob).withFlex(1.0).withMargin(2));
+    flex.performLayout(juce::Rectangle<int>(7, 17, getWidth() - 14, getHeight() - 28));
+}
--- src/juceplugin/modulecomponents.h
+++ src/juceplugin/modulecomponents.h
@@ -318,67 +318,8 @@ class OscTypeComponent : public juce::Component, public juce::Timer
             repaint();
         }
     }
-    void paint(juce::Graphics &g) override
-    {
-        g.fillAll(juce::Colours::black);
-        int otypebase = *processorRef.granulator.idtoparvalptr[ToneGranulator::PAR_OSCTYPE];
-        float cellw = 50.0f;
-        for (int i = 0; i < 7; ++i)
-        {
-            if (i == otypebase)
-                g.setColour(juce::Colours::yellow.darker());
-            else
-                g.setColour(juce::Colours::darkgrey);
-            juce::Rectangle<float> r{cellw * i + 2.0f, 2.0f, cellw - 4.0f, cellw - 4.0f};
-            g.fillRect(r);
-
-            g.setColour(juce::Colours::white);
-            int mappedtype = processorRef.granulator.osctypemapping[i];
-            if (mappedtype >= 0 && mappedtype < waveimages.size())
-            {
-                g.drawImage(waveimages[mappedtype], cellw * i + 2.0, 2.0f, cellw - 4.0f,
-                            cellw - 4.0f, 0, 0, 50, 50);
-            }
-            if (i == priorOscType)
-            {
-                g.setColour(juce::Colours::green);
-                g.fillRect(cellw * i + 4.0f, 4.0f, 8.0f, 8.0f);
-            }
-            // g.drawFittedText(waveshortnames[mappedtype], r.getX(), r.getY(), r.getWidth(),
-            //                  r.getHeight(), juce::Justification::centred, 1);
-        }
-    }
-    void mouseDown(const juce::MouseEvent &ev) override
-    {
-        int otype = ev.x / 50.0;
-        if (otype >= 0 && otype < 7)
-        {
-            if (!ev.mods.isRightButtonDown())
-            {
-                ParameterMessage msg;
-                msg.id = ToneGranulator::PAR_OSCTYPE;
-                msg.value = otype;
-                processorRef.params_from_gui_fifo.push(msg);
-            }
-            else
-            {
-                juce::PopupMenu menu;
-                for (int i = 0; i < 7; ++i)
-                {
-                    auto dr = std::make_unique<juce::DrawableImage>(waveimages[i]);
-                    menu.addItem(juce::PopupMenu::Item(waveshortnames[i])
-                                     .setImage(std::move(dr))
-                                     .setID(i + 1)
-                                     .setAction([this, otype, i] {
-                                         processorRef.granulator.osctypemapping[otype] = i;
-                                         repaint();
-                                     }));
-                }
-                menu.showMenuAsync(juce::PopupMenu::Options{}.withStandardItemHeight(50));
-            }
-        }
-        repaint();
-    }
+    void paint(juce::Graphics &g) override;
+    void mouseDown(const juce::MouseEvent &ev) override;
 };
 
 class OscillatorModuleComponent : public juce::GroupComponent
@@ -446,37 +387,7 @@ class OscillatorModuleComponent : public juce::GroupComponent
         addChildComponent(oscTypeEditor);
         oscTypeEditor.setBounds(2, 2, 200, 25);
     }
-    void resized() override
-    {
-        oscTypeComponent.setBounds(7, 17, 350, 50);
-        oscPitchKnob.setBounds(7, oscTypeComponent.getBottom() + 1, 80, 100);
-
-        for (int i = 0; i < modDepthKnobs.size(); ++i)
-        {
-            modDepthKnobs[i]->setBounds(oscPitchKnob.getRight() + 2,
-                                        oscTypeComponent.getBottom() + 1 + i * 51, 80, 50);
-        }
-
-        // pitchEnvWarpKnob.setBounds(oscPitchKnob.getRight() + 2, pitchEnvKnob.getBottom() + 1, 80,
-        //                            50);
-        pitchEnvelopeComponent.setBounds(modDepthKnobs[0]->getRight() + 2,
-                                         oscTypeComponent.getBottom(), 150, 150);
-        grainModComponent.setBounds(pitchEnvelopeComponent.getRight() + 2,
-                                    oscTypeComponent.getBottom(), 150, 150);
-        oscSyncKnob.setBounds(grainModComponent.getRight() + 2, oscTypeComponent.getBottom() + 1,
-                              80, 50);
-        oscPWKnob.setBounds(grainModComponent.getRight() + 2, oscSyncKnob.getBottom() + 1, 80, 50);
-        oscFMPitchKnob.setBounds(oscSyncKnob.getRight() + 2, oscTypeComponent.getBottom() + 1, 80,
-                                 50);
-        oscFMDepthKnob.setBounds(oscFMPitchKnob.getRight() + 2, oscTypeComponent.getBottom() + 1,
-                                 80, 50);
-        oscFMFeedbackKnob.setBounds(oscFMDepthKnob.getRight() + 2, oscTypeComponent.getBottom() + 1,
-                                    80, 50);
-        oscNoiseModeDrop.setBounds(oscSyncKnob.getRight() + 2, oscFMPitchKnob.getBottom() + 1, 250,
-                                   25);
-        oscNoiseCorrelationKnob.setBounds(oscSyncKnob.getRight() + 2,
-                                          oscNoiseModeDrop.getBottom() + 1, 80, 50);
-    }
+    void resized() override;
 };
 
 class StackingModuleComponent : public juce::GroupComponent
@@ -508,17 +419,7 @@ class StackingModuleComponent : public juce::GroupComponent
         initSlider(p, *this, pitchRandomKnob);
         initSlider(p, *this, spatRandomKnob);
     }
-    void resized() override
-    {
-        juce::FlexBox flex;
-        flex.flexDirection = juce::FlexBox::Direction::row;
-        flex.items.add(juce::FlexItem(countKnob).withFlex(1.0).withMargin(2));
-        flex.items.add(juce::FlexItem(lengthKnob).withFlex(1.0).withMargin(2));
-        flex.items.add(juce::FlexItem(warpKnob).withFlex(1.0).withMargin(2));
-        flex.items.add(juce::FlexItem(pitchRandomKnob).withFlex(1.0).withMargin(2));
-        flex.items.add(juce::FlexItem(spatRandomKnob).withFlex(1.0).withMargin(2));
-        flex.performLayout(juce::Rectangle<int>(7, 17, getWidth() - 14, getHeight() - 28));
-    }
+    void resized() override;
 };
 
 class PerformanceComponent : public juce::Component, public juce::Timer
