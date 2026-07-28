7a4433f7808fb0f1e1bbbe9a327e411ecf7bae79
xenakios <xenakios@gmail.com>
2026-07-28 22:06:30 +0300

    another approach for the spectrogram but this is slow and doesn't work correctly

--- src/juceplugin/audiovisualizercomponent.cpp
+++ src/juceplugin/audiovisualizercomponent.cpp
@@ -1,51 +1,49 @@
 #include "audiovisualizercomponent.h"
 #include "juce_audio_basics/juce_audio_basics.h"
 
-void XenAudioVisualizerComponent::drawNextLineOfSpectrogram()
+void XenAudioVisualizerComponent::timerCallback()
+{
+    jassert(sampleRate > 0.0f);
+    drawNextLinesOfSpectrogram();
+    repaint();
+}
+
+void XenAudioVisualizerComponent::drawNextLinesOfSpectrogram()
 {
     auto rightHandEdge = spectrogramImage.getWidth() - 1;
     auto imageHeight = spectrogramImage.getHeight();
-
-    // first, shuffle our image leftwards by 1 pixel..
-    spectrogramImage.moveImageSection(0, 0, 1, 0, rightHandEdge, imageHeight);
-    std::array<float, 2 * fftSize> localFFTData;
-    {
-        const juce::SpinLock::ScopedLockType sl(fftDataLock);
-        localFFTData = fftDataSnapshot;
-    }
-    forwardFFT.performFrequencyOnlyForwardTransform(localFFTData.data());
-    // then render our FFT data..
-    // --- log-frequency axis parameters ---
     const auto binWidth = (float)sampleRate / (float)fftSize; // Hz per FFT bin
     const auto minFreq = binWidth;                            // skip the DC bin
     const auto maxFreq = (float)sampleRate * 0.5f;            // Nyquist
 
-    juce::Image::BitmapData bitmap{
-        spectrogramImage, rightHandEdge, 0, 1, imageHeight, juce::Image::BitmapData::writeOnly};
-    const bool measure_level_range = false;
-    if (measure_level_range)
+    std::array<float, fftSize> temp;
+    double t0 = juce::Time::getMillisecondCounterHiRes();
+    if (mainfifo.pop(temp))
     {
-        auto lo = 1000.0f, hi = -1000.0f;
-        for (int i = 0; i < fftSize / 2; ++i)
+        // first, shuffle our image leftwards by 1 pixel..
+        spectrogramImage.moveImageSection(0, 0, 1, 0, rightHandEdge, imageHeight);
+        juce::Image::BitmapData bitmap{
+            spectrogramImage, rightHandEdge, 0, 1, imageHeight, juce::Image::BitmapData::writeOnly};
+        std::array<float, 2 * fftSize> localFFTData;
+        for (int i = 0; i < fftSize; ++i)
         {
-            auto db = juce::Decibels::gainToDecibels(localFFTData[(size_t)i]);
-            lo = juce::jmin(lo, db);
-            hi = juce::jmax(hi, db);
+            localFFTData[i] = temp[i];
         }
-        DBG(lo << " to " << hi);
+        forwardFFT.performFrequencyOnlyForwardTransform(localFFTData.data());
+        for (auto y = 1; y < imageHeight; ++y)
+        {
+            // top of image = high frequency, bottom = low frequency
+            auto proportionY = 1.0f - (float)y / (float)imageHeight;
+            auto freq = minFreq * std::pow(maxFreq / minFreq, proportionY);
+            auto fftDataIndex = juce::jlimit(0, fftSize / 2, (int)std::round(freq / binWidth));
+            const float fixedMinLevel = -70.0f;
+            const float fixedMaxLevel = 70.0f;
+            float leveldb = juce::Decibels::gainToDecibels(localFFTData[fftDataIndex]);
+            auto level = juce::jlimit(
+                0.0f, 1.0f, juce::jmap(leveldb, fixedMinLevel, fixedMaxLevel, 0.0f, 1.0f));
+            bitmap.setPixelColour(0, y, magmaColour(level));
+        }
     }
-
-    for (auto y = 1; y < imageHeight; ++y)
-    {
-        // top of image = high frequency, bottom = low frequency
-        auto proportionY = 1.0f - (float)y / (float)imageHeight;
-        auto freq = minFreq * std::pow(maxFreq / minFreq, proportionY);
-        auto fftDataIndex = juce::jlimit(0, fftSize / 2, (int)std::round(freq / binWidth));
-        const float fixedMinLevel = -70.0f;
-        const float fixedMaxLevel = 70.0f;
-        float leveldb = juce::Decibels::gainToDecibels(localFFTData[fftDataIndex]);
-        auto level =
-            juce::jlimit(0.0f, 1.0f, juce::jmap(leveldb, fixedMinLevel, fixedMaxLevel, 0.0f, 1.0f));
-        bitmap.setPixelColour(0, y, magmaColour(level));
-    }
+    double t1 = juce::Time::getMillisecondCounterHiRes();
+    // DBG("popping fifo took " + juce::String(t1 - t0, 1) + " milliseconds");
 }
--- src/juceplugin/audiovisualizercomponent.h
+++ src/juceplugin/audiovisualizercomponent.h
@@ -3,44 +3,37 @@
 #include "juce_graphics/juce_graphics.h"
 #include "juce_gui_basics/juce_gui_basics.h"
 #include "juce_dsp/juce_dsp.h"
+#include "containers/choc_SingleReaderSingleWriterFIFO.h"
 
 class XenAudioVisualizerComponent : public juce::Component, public juce::Timer
 {
   public:
-    static constexpr auto fftOrder = 11;
+    static constexpr auto fftOrder = 10;
     static constexpr auto fftSize = 1 << fftOrder;
-    static constexpr auto hopSize = fftSize / 4; // 75% overlap — try /2 for 50% first
-
+    static constexpr auto hopSize = fftSize / 1; // 75% overlap — try /2 for 50% first
+    static const int scrollamount = 4;
+    choc::fifo::SingleReaderSingleWriterFIFO<std::array<float, fftSize>> mainfifo;
     std::vector<float> ringBuffer{std::vector<float>(fftSize, 0.0f)};
     int ringBufferIndex = 0;
     int samplesSinceLastFFT = 0;
-    juce::SpinLock fftDataLock;
+
     alignas(16) std::array<float, 2 * fftSize> fftDataSnapshot;
     juce::dsp::FFT forwardFFT;
     juce::dsp::WindowingFunction<float> window;
     juce::Image spectrogramImage;
 
-    alignas(16) std::atomic<bool> nextFFTBlockReady = false;
     float sampleRate = 0.0f;
     XenAudioVisualizerComponent()
         : forwardFFT(fftOrder), window(fftSize, juce::dsp::WindowingFunction<float>::hann),
           spectrogramImage(juce::Image::RGB, 1024, 1024, true)
     {
+        mainfifo.reset(32);
         setOpaque(true);
         startTimerHz(60);
     }
     void setSampleRate(float sr) { sampleRate = sr; }
-    void timerCallback() override
-    {
-        jassert(sampleRate > 0.0f);
-        if (nextFFTBlockReady)
-        {
-            drawNextLineOfSpectrogram();
-            nextFFTBlockReady = false;
-            repaint();
-        }
-    }
-    void drawNextLineOfSpectrogram();
+    void timerCallback() override;
+    void drawNextLinesOfSpectrogram();
     void paint(juce::Graphics &g) override
     {
         g.fillAll(juce::Colours::black);
@@ -48,6 +41,9 @@ class XenAudioVisualizerComponent : public juce::Component, public juce::Timer
         g.setOpacity(1.0f);
         // g.setImageResamplingQuality(juce::Graphics::highResamplingQuality);
         g.drawImage(spectrogramImage, getLocalBounds().toFloat());
+        g.setColour(juce::Colours::yellow);
+        g.drawText(juce::String(mainfifo.getUsedSlots()), 1, 1, 100, 25,
+                   juce::Justification::centredLeft);
     }
     static juce::Colour magmaColour(float level) noexcept
     {
@@ -73,22 +69,14 @@ class XenAudioVisualizerComponent : public juce::Component, public juce::Timer
     {
         ringBuffer[(size_t)ringBufferIndex] = sample;
         ringBufferIndex = (ringBufferIndex + 1) % fftSize;
-
         if (++samplesSinceLastFFT >= hopSize)
         {
             samplesSinceLastFFT = 0;
-
-            std::array<float, 2 * fftSize> localData{};
+            std::array<float, fftSize> localData{};
             for (int i = 0; i < fftSize; ++i)
                 localData[(size_t)i] = ringBuffer[(size_t)((ringBufferIndex + i) % fftSize)];
-
             window.multiplyWithWindowingTable(localData.data(), fftSize);
-
-            {
-                const juce::SpinLock::ScopedLockType sl(fftDataLock);
-                fftDataSnapshot = localData;
-            }
-            nextFFTBlockReady = true;
+            mainfifo.push(localData);
         }
     }
 };
