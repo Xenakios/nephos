1bb69534a1f212e79a932a6d0339c9f6ce93cff4
xenakios <xenakios@gmail.com>
2026-08-10 15:32:04 +0300

    move step sequencer gui code to other files

--- src/juceplugin/modulecomponents.cpp
+++ src/juceplugin/modulecomponents.cpp
@@ -811,3 +811,341 @@ void TimeModuleComponent::resized()
     flex.items.add(juce::FlexItem(tailKnob).withFlex(1.0).withMargin(2));
     flex.performLayout(juce::Rectangle<int>(7, 41, getWidth() - 14, getHeight() - 49));
 }
+
+
+choc::value::Value get_js_info(std::string jscode);
+void cancel_js();
+std::vector<float> generate_from_js(std::string jscode, std::vector<float> currentsteps,
+                                    int startstep, int endstep, std::vector<float> params);
+choc::value::Value perform_js(std::string jscode, choc::value::ValueView info);
+
+
+void StepSeqComponent::paint(juce::Graphics &g)
+{
+    g.fillAll(juce::Colours::black);
+    auto &msrc = gr->stepModSources[sindex];
+    int maxstepstodraw = (getWidth() - graphxpos) / 16;
+    int stepstodraw = std::min<int>(maxstepstodraw, msrc.numactivesteps);
+    for (int i = 0; i < maxstepstodraw; ++i)
+    {
+        float xcor = graphxpos + i * 16.0;
+        float v = msrc.steps[i];
+        if (msrc.unipolar)
+            v = (v + 1.0) * 0.5;
+        if (i == playingStep)
+            g.setColour(juce::Colours::white);
+        else
+        {
+            if (i >= msrc.loopstartstep && i < msrc.loopstartstep + msrc.looplen)
+                g.setColour(juce::Colours::green);
+            else
+                g.setColour(juce::Colours::darkgreen.darker());
+        }
+
+        if (v < 0.0)
+        {
+            float h = juce::jmap<float>(v, -1.0, 0.0, getHeight() / 2.0, 0.0);
+            g.fillRect(xcor, getHeight() / 2.0, 15.0, h);
+        }
+        else
+        {
+
+            float h = juce::jmap<float>(v, 0.0, 1.0, 0.0, getHeight() / 2.0);
+            g.fillRect(xcor, getHeight() / 2.0 - h, 15.0, h);
+        }
+    }
+    g.setColour(juce::Colours::white);
+    g.drawRect(graphxpos + editRange.getStart() * 16.0, 1.0f, editRange.getLength() * 16.0,
+               (float)getHeight() - 2.0f, 2.0f);
+    g.setColour(juce::Colours::cyan);
+    float xcor = graphxpos + (msrc.loopstartstep + msrc.loopoffset) * 16.0;
+    g.drawLine(xcor, 0, xcor, getHeight(), 3.0f);
+    g.setColour(juce::Colours::black);
+    g.drawLine(float(graphxpos), getHeight() / 2.0f, getWidth(), getHeight() / 2.0f);
+    juce::String txt;
+    txt << editRange.getStart() << " " << editRange.getEnd();
+    if (autoSetLoop)
+        txt << "\nLOOP FOLLOWS SELECTION";
+    juce::GenericScopedLock locker(spinlock);
+    if (!js_error.empty())
+        txt << "\nJavaScript error : " << js_error;
+    g.setColour(juce::Colours::white);
+    g.drawMultiLineText(txt, graphxpos + 2, 20, getWidth() - graphxpos - 3);
+}
+
+bool StepSeqComponent::keyPressed(const juce::KeyPress &ev)
+{
+    auto &msrc = gr->stepModSources[sindex];
+    int curstart = msrc.loopstartstep;
+    int curlooplen = msrc.looplen;
+
+    int actiontaken = 0;
+    auto incdecstep = [this, &msrc](float step) {
+        int index = editRange.getStart();
+        float v = msrc.steps[index];
+        v = std::clamp(v + step, -1.0f, 1.0f);
+        gr->fifo.push({StepModSource::Message::OP_SETSTEP, sindex, v, index});
+    };
+    if (ev.getKeyCode() == 'R')
+    {
+        runJSInThread();
+        return true;
+        scriptParamsEditor.setVisible(true);
+        scriptParamsEditor.setBounds(graphxpos + 1, 1, 150, 25);
+        scriptParamsEditor.grabKeyboardFocus();
+        scriptParamsEditor.onReturnKey = [this]() {
+            scriptParamsEditor.setVisible(false);
+            runJSInThread();
+        };
+        scriptParamsEditor.onEscapeKey = [this]() { scriptParamsEditor.setVisible(false); };
+    }
+    else if (ev.getKeyCode() == 'Y')
+    {
+        autoSetLoop = !autoSetLoop;
+        actiontaken = 1;
+    }
+    else if (ev.getKeyCode() == 'P')
+    {
+        runExternalProgram();
+        actiontaken = 2;
+    }
+    else if (ev.getKeyCode() == 'T')
+    {
+        incdecstep(0.1f);
+        actiontaken = 2;
+    }
+    else if (ev.getKeyCode() == 'G')
+    {
+        incdecstep(-0.1f);
+        actiontaken = 2;
+    }
+    else if (ev.getKeyCode() == 'Z')
+    {
+        int curoff = msrc.loopoffset;
+        curoff = (curoff + 1) % msrc.looplen;
+        gr->fifo.push({StepModSource::Message::OP_OFFSET, sindex, 0.0f, curoff});
+        actiontaken = 2;
+    }
+    else if (ev.getKeyCode() == 'E')
+    {
+        setLoopFromSelection();
+        actiontaken = 2;
+    }
+    else if (ev.getKeyCode() == 'D' && ev.getModifiers() == juce::ModifierKeys::noModifiers)
+    {
+        for (int i = 0; i < editRange.getLength(); ++i)
+        {
+            int index = editRange.getStart() + i;
+            gr->fifo.push({StepModSource::Message::OP_SETSTEP, sindex,
+                           rng.nextFloatInRange(-1.0f, 1.0f), index});
+        }
+        actiontaken = 2;
+    }
+    else if (ev.getKeyCode() == 'D' && ev.getModifiers().isShiftDown())
+    {
+        if (editRange.getLength() > 1)
+        {
+            for (int i = 0; i < editRange.getLength(); ++i)
+            {
+                int index = editRange.getStart() + i;
+                float v = -1.0 + 2.0 / (editRange.getLength() - 1) * i;
+                gr->fifo.push({StepModSource::Message::OP_SETSTEP, sindex, v, index});
+            }
+        }
+        else
+        {
+            gr->fifo.push({StepModSource::Message::OP_SETSTEP, sindex, 0.0f, editRange.getStart()});
+        }
+
+        actiontaken = 2;
+    }
+    else if (ev.getKeyCode() == 'Q' && ev.getModifiers() == juce::ModifierKeys::noModifiers)
+    {
+        editRange = editRange.movedToStartAt(editRange.getStart() + 1);
+        actiontaken = 1;
+    }
+    else if (ev.getKeyCode() == 'Q' && ev.getModifiers().isShiftDown())
+    {
+        if (editRange.getLength() > 1)
+            editRange.setStart(editRange.getStart() + 1);
+        actiontaken = 1;
+    }
+    else if (ev.getKeyCode() == 'A' && ev.getModifiers() == juce::ModifierKeys::noModifiers)
+    {
+        editRange = editRange.movedToStartAt(editRange.getStart() - 1);
+        actiontaken = 1;
+    }
+    else if (ev.getKeyCode() == 'A' && ev.getModifiers().isShiftDown())
+    {
+        if (editRange.getStart() > 0)
+            editRange.setStart(editRange.getStart() - 1);
+        actiontaken = 1;
+    }
+
+    else if (ev.getKeyCode() == 'W')
+    {
+        editRange = editRange.withLength(editRange.getLength() + 1);
+        actiontaken = 1;
+    }
+    else if (ev.getKeyCode() == 'S')
+    {
+        editRange = editRange.withLength(editRange.getLength() - 1);
+        actiontaken = 1;
+    }
+    editRange = juce::Range<int>{0, 4096}.constrainRange(editRange);
+    if (editRange.getLength() < 1)
+        editRange.setLength(1);
+    if (actiontaken == 1 && autoSetLoop)
+    {
+        setLoopFromSelection();
+    }
+    return actiontaken > 0;
+}
+
+StepSeqComponent::StepSeqComponent(int seqindex, ToneGranulator *g, juce::ThreadPool *tp)
+    : gr(g), sindex(seqindex), threadPool(tp)
+{
+    addChildComponent(scriptParamsEditor);
+    rng.seed(11400714819323198485ULL, 17 + sindex * 31);
+    editRange.setStart(0);
+    editRange.setLength(g->stepModSources[sindex].looplen);
+    setWantsKeyboardFocus(true);
+
+    addAndMakeVisible(cancelButton);
+    cancelButton.setButtonText("Stop JS script");
+    cancelButton.onClick = [this]() {
+        cancel_js();
+        cancelButton.setVisible(false);
+    };
+    cancelButton.setVisible(false);
+
+    addAndMakeVisible(unipolarBut);
+    unipolarBut.setButtonText("Unipolar");
+    unipolarBut.onClick = [this]() {
+        gr->fifo.push(
+            {StepModSource::Message::OP_UNIPOLAR, sindex, 0.0f, unipolarBut.getToggleState()});
+    };
+    addAndMakeVisible(par0Slider);
+    par0Slider.setRange(0.0, 1.0);
+    par0Slider.setNumDecimalPlacesToDisplay(2);
+    par0Slider.onDragEnd = [this]() { runExternalProgram(); };
+}
+
+void StepSeqComponent::runJSInThread()
+{
+    js_error = "";
+    auto jscode = choc::file::loadFileAsString(R"(C:\develop\nephos\src\generatesteps.js)");
+    auto info = get_js_info(jscode);
+    if (!info.hasObjectMember("error"))
+    {
+        if (!jsSettingsComponent)
+            jsSettingsComponent = std::make_unique<JSEntryComponent>(info);
+        jsSettingsComponent->OnRun = [this, jscode]() {
+            auto ob = choc::value::createObject("");
+            for (auto &ed : jsSettingsComponent->editors)
+            {
+                auto id = jsSettingsComponent->editorToProperty[ed.get()];
+                ob.setMember(id, ed->getText().getDoubleValue());
+            }
+            try
+            {
+                auto r = perform_js(jscode, ob);
+                // DBG(choc::json::toString(r));
+                if (r.hasObjectMember("steps"))
+                {
+                    auto arr = r["steps"];
+                    for (int i = 0; i < arr.size(); ++i)
+                    {
+                        float val = arr[i].getWithDefault(0.0);
+                        gr->fifo.push({StepModSource::Message::OP_SETSTEP, sindex, val, (int)i});
+                    }
+                }
+            }
+            catch (std::exception &ex)
+            {
+                DBG(ex.what());
+            }
+        };
+        jsSettingsComponent->OnHide = [this]() { jsSettingsComponent->setVisible(false); };
+        getParentComponent()->getParentComponent()->addAndMakeVisible(*jsSettingsComponent);
+        int h = jsSettingsComponent->labels.size() * 30 + 60;
+        jsSettingsComponent->setBounds(0, 0, 500, h);
+        return;
+    }
+    else
+    {
+        js_error = info["error"].getWithDefault("");
+        repaint();
+        return;
+    }
+    auto tokens = juce::StringArray::fromTokens(scriptParamsEditor.getText(), true);
+    if (tokens.size() < 1 || tokens.size() > 1024)
+    {
+        js_error = "There must be more than 0 and less 1024 tokens in parameters";
+        return;
+    }
+    js_error = "";
+    js_status.store(1);
+    cancelButton.setVisible(true);
+    std::vector<float> params;
+    params.reserve(tokens.size());
+    for (int i = 0; i < tokens.size(); ++i)
+    {
+        params.push_back(tokens[i].getFloatValue());
+    }
+
+    threadPool->addJob([this, params]() {
+        try
+        {
+            auto jscode = choc::file::loadFileAsString(R"(C:\develop\nephos\src\generatesteps.js)");
+            auto steps = gr->stepModSources[sindex].steps;
+            steps =
+                generate_from_js(jscode, steps, editRange.getStart(), editRange.getEnd(), params);
+            for (size_t i = 0; i < steps.size(); ++i)
+            {
+                gr->fifo.push({StepModSource::Message::OP_SETSTEP, sindex, steps[i],
+                               (int)i + editRange.getStart()});
+            }
+        }
+        catch (std::exception &ex)
+        {
+            DBG(ex.what());
+            juce::GenericScopedLock locker(spinlock);
+            js_error = ex.what();
+        }
+        js_status.store(0);
+        juce::MessageManager::callAsync([this]() { cancelButton.setVisible(false); });
+    });
+}
+
+void StepSeqComponent::runExternalProgram()
+{
+    threadPool->addJob([this]() {
+        juce::ChildProcess cp;
+        double t0 = juce::Time::getMillisecondCounterHiRes();
+        cp.start(fmt::format(
+            R"(python C:\develop\AudioPluginHost_mk2\Source\granularsynth\stepseq.py {} {} {})",
+            sindex, editRange.getStart(), editRange.getLength()));
+        auto data = cp.readAllProcessOutput();
+        double t1 = juce::Time::getMillisecondCounterHiRes();
+        DBG("running ext program took " << t1 - t0 << " millisecods");
+        if (!data.containsIgnoreCase("error"))
+        {
+            auto tokens = juce::StringArray::fromTokens(data, false);
+            for (int i = 0; i < tokens.size(); ++i)
+            {
+                if (tokens[i].isEmpty() || i == 4096 || i > editRange.getLength())
+                    break;
+                float v = std::clamp(tokens[i].getFloatValue(), -1.0f, 1.0f);
+                // DBG(v);
+                int index = editRange.getStart() + i;
+                gr->fifo.push({StepModSource::Message::OP_SETSTEP, sindex, v, index});
+            }
+        }
+        else
+        {
+            DBG(data);
+        }
+    });
+}
+
