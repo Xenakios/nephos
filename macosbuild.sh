60787d6488b954a8359922cc15ba10817cc8d704
xenakios <xenakios@gmail.com>
2026-09-21 20:48:59 +0300

    edited macos build notes

--- macosbuild.sh
+++ macosbuild.sh
@@ -9,8 +9,10 @@ cmake .. \
   -DCMAKE_BUILD_TYPE=Release
 
 cmake --build . --target NephosPlugin_VST3
-# clone into libs : choc, clap, clap-helpers, fmt, juce, simde, xenakios sst-basic-blocks, sst-cpputils
-# sst-filters, git clone https://github.com/surge-synthesizer/tuning-library.git
+# clone into libs : choc, clap, clap-helpers, fmt, juce, simde, 
+# git clone https://github.com/Xenakios/sst-basic-blocks.git
+# sst-cpputils 
+# sst-filters, 
+# git clone https://github.com/surge-synthesizer/tuning-library.git
 # JUCE commit 91ad83ae34a81e0833b1a2b0866f54846370ae53
-#  https://github.com/Xenakios/sst-basic-blocks.git
 # fmt commit b5c4e795bba50793eee7e4ccce44a48c3310adca
